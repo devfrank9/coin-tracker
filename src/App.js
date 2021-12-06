420be2d6a8e6450e1a488fefba8b08a5b8d9e89d
Jongho Lee <90244663+DevFrank9@users.noreply.github.com>
2021-12-07 04:54:25 +0900

    theme(다크모드) 적용 맛보기

--- src/App.js
+++ src/App.js
@@ -1,49 +1,21 @@
 import styled, { keyframes } from "styled-components";
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
 const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   height: 100vh;
-`;
-
-const rotationAnimation = keyframes`
-0%{
-  transform:rotate(0deg);
-  border-radius:0px;
-}
-50%{
-  border-radius:100px;
-}
-100%{
-  transform:rotate(360deg);
-  border-radius:0px;
-}
-`;
-
-const Emoji = styled.span`
-  font-size: 30px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  animation: ${rotationAnimation} 1s linear infinite;
-  ${Emoji}:hover {
-    font-size: 98px;
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as="p">hey!</Emoji>
-      </Box>
+      <Title>Hello!</Title>
     </Wrapper>
   );
 }
