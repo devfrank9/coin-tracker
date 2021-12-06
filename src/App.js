5fae182d8dc6832ebcef17edf23f8faa65a609de
Jongho Lee <90244663+DevFrank9@users.noreply.github.com>
2021-12-07 02:34:17 +0900

    styled-components를 이용해서 이전 beginner 코스 복습

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import "./App.css";
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Father>
+      <Box bgColor="teal" />
+      <Circle bgColor="whitesmoke" />
+    </Father>
+  );
 }
 
 export default App;
