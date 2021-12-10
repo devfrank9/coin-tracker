bdeb8105e5a7e2fcab893d9d65f6782d3c55b578
Jongho Lee <90244663+DevFrank9@users.noreply.github.com>
2021-12-11 02:04:48 +0900

    Coin tracker 만들기 시작. route구성하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
 import styled from "styled-components";
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-
-const H1 = styled.div`
-  color: ${(props) => props.theme.textColor};
-`;
+import Router from "./Router";
 
 function App() {
-  return (
-    <Container>
-      <H1>protected</H1>
-    </Container>
-  );
+  return <Router />;
 }
 
 export default App;
