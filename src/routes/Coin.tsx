a39d444bfac899b53757bd5fceeeaf8c9554c957
Jongho Lee <90244663+DevFrank9@users.noreply.github.com>
2021-12-30 13:30:17 +0900

    chart api불러와서 넣기

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,5 +1,4 @@
 // eslint-disable-next-line
-import { useState, useEffect } from "react";
 import { useQuery } from "react-query";
 import {
   Switch,
@@ -158,22 +157,6 @@ function Coin() {
     () => fetchCoinTickers(coinId)
   );
 
-  /*   const [loading, setLoading] = useState(true);
-  const [info, setInfo] = useState<InfoData>();
-  const [priceInfo, setPriceInfo] = useState<PriceData>();
-  useEffect(() => {
-    (async () => {
-      const infoData = await (
-        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
-      ).json();
-      const priceData = await (
-        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
-      ).json();
-      setInfo(infoData);
-      setPriceInfo(priceData);
-      setLoading(false);
-    })();
-  }, [coinId]); */
   const loading = infoLoading || tickersLoading;
 
   return (
@@ -227,7 +210,7 @@ function Coin() {
               <Price />
             </Route>
             <Route path={`/:coinId/chart`}>
-              <Chart />
+              <Chart coinId={coinId} />
             </Route>
           </Switch>
         </>
